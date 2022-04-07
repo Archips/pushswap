c41dc9555900f790391989a26d91a939224884f5
Archibald Thirion <archibald@archibalds-macbook-pro.home>
2022-04-07 10:26:31 +0200

    New checker in progress

--- checker/checker.c
+++ checker/checker.c
@@ -6,120 +6,89 @@
 /*   By: athirion <athirion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 16:46:40 by athirion          #+#    #+#             */
-/*   Updated: 2022/04/04 13:29:48 by athirion         ###   ########.fr       */
+/*   Updated: 2022/04/07 09:39:41 by athirion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
 
-int	ft_operation_index(char *op)
+static void	ft_init_stacks(t_data *data)
 {
-	int	index;
+	int	i;
+	int	*tab_ptr;
 
-	index = -1;
-	index = 0 * (ft_strncmp(op, "sa", 2) == 0)
-		+ 1 * (ft_strncmp(op, "sb", 2) == 0)
-		+ 2 * (ft_strncmp(op, "ss", 2) == 0)
-		+ 3 * (ft_strncmp(op, "pa", 2) == 0)
-		+ 4 * (ft_strncmp(op, "sb", 2) == 0)
-		+ 5 * (ft_strncmp(op, "ra", 2) == 0)
-		+ 6 * (ft_strncmp(op, "rb", 2) == 0)
-		+ 7 * (ft_strncmp(op, "rr", 2) == 0)
-		+ 8 * (ft_strncmp(op, "rra", 3) == 0)
-		+ 9 * (ft_strncmp(op, "rrb", 3) == 0)
-		+ 10 * (ft_strncmp(op, "rrr", 3) == 0);
-	if (index == -1)
-	{
-		ft_putstr_fd("Error\n", 2);
-		exit(EXIT_FAILURE);
-	}
-	return (index);
+	data->stack_b = ft_listnew(0);
+	tab_ptr = data->tab;
+	data->stack_a = ft_listnew(*(data)->tab++);
+	i = 0;
+	while (i++ < (int)data->tab_size - 1)
+		ft_listadd_back(&data->stack_a, ft_listnew(*(data)->tab++));
+	data->tab = tab_ptr;
+	data->oplist = ft_char_listnew("init");
 }
 
-void	ft_exit(int success, t_lst *stack_a, t_lst *stack_b)
+void	ft_get_operations(t_data *data, char *op)
 {
-	ft_listclear(&stack_a, free);
-	ft_listclear(&stack_b, free);
-	if (success)
+	if ft_strncmp(op, "sa\n", 3) == 0)
+		ft_swap(data->stack_a, NULL, NULL);
+	else if ft_strncmp(op, "sb\n", 3) == 0)
+		ft_swap(data->stack_b, NULL, NULL);	
+	else if ft_strncmp(op, "ss\n", 3) == 0)
+		ft_ss(data->stack_a, data->stack_b, NULL);
+	else if ft_strncmp(op, "pa\n", 3) == 0)
+		ft_push(data->stack_a, data->stack_b, NULL, NULL);
+	else if ft_strncmp(op, "pb\n", 3) == 0)
+		ft_push(data->stack_b, data->stack_a, NULL, NULL);
+	else if ft_strncmp(op, "ra\n", 3) == 0)
+		ft_rotate(data->stack_a, NULL, NULL);
+	else if ft_strncmp(op, "rb\n", 3) == 0)
+		ft_rotate(data->stack_b, NULL, NULL);
+	else if ft_strncmp(op, "rr\n", 3) == 0)
+		ft_rr(data->stack_a, data->stack_b, NULL, NULL);
+	else if ft_strncmp(op, "rra\n", 4) == 0)
+		ft_reverse_rotate(data->stack_a, NULL, NULL);
+	else if ft_strncmp(op, "rrb\n", 4) == 0)
+		ft_reverse_rotate(data->stack_b, NULL, NULL);
+	else if ft_strncmp(op, "rrr\n", 4) == 0)
+		ft_reverse_rotate(data->stack_a, data->stack_b, NULL, NULL);
+	else
+		exit(EXIT_FAILURE); //TO DO
+}
+
+void	ft_read_operation(t_data *data)
+{
+	char *op;
+
+	while (1)
+	{
+		op = get_next_line(0);
+		if (!op)
+			exit(EXIT_FAILURE); //TO DO
+		ft_get_operations(data, op);
+		free(op);
+	}
+	if (ft_listissort(data->stack_a) && ft_set_ids(data->stack_b) == 0)
+		ft_putstr_fd("OK\n", 1);
 		exit(EXIT_SUCCESS);
+	ft_putstr_fd("KO\n", 1);
 	exit(EXIT_FAILURE);
 }
 
-void	ft_ope_tab(void (*ope[11](void *)))
+int	main(int ac, char **av)
 {
-	ope[0] = (void *)ft_sa;
-	ope[1] = (void *)ft_sb;
-	ope[2] = (void *)ft_ss;
-	ope[3] = (void *)ft_pa;
-	ope[4] = (void *)ft_pb;
-	ope[5] = (void *)ft_ra;
-	ope[6] = (void *)ft_rb;
-	ope[7] = (void *)ft_rr;
-	ope[8] = (void *)ft_rra;
-	ope[9] = (void *)ft_rrb;
-	ope[10] = (void *)ft_rrr;
-}
+	t_data	data;
 
-int	ft_stack_len(t_lst *stack)
-{
-	t_lst	*temp;
-	int		len;
-
-	temp = stack;
-	len = 0;
-	while (temp && temp->next)
+	if (ac > 1)
 	{
-		len ++;
-		temp = temp->next;
+		ft_parser(&data, ac, av);
+		if (!data.tab)
+			return (ft_putstr_fd("Error\n", 2), 1);
+		ft_init_stacks(&data);
+		if (!ft_listissort(data.stack_a))
+			ft_read_operations(&data);
+		ft_listclear(&data.stack_a, free);
+		ft_listclear(&data.stack_b, free);
+		ft_free(data.tab);
 	}
-	return (len);
-}
-
-int	ft_issort(t_lst *stack)
-{
-	t_lst *temp_1;
-	t_lst *temp_2;
-
-	temp_1 = stack;
-	while (temp_1)
-	{
-		temp_2 = temp_1->next;
-		while (temp_2)
-		{
-			if (temp_1->content > temp_2->content)
-				return (0);
-			temp_2 = temp_2->next;
-		}
-		temp_1 = temp_1->next;
-	}
-	return (1);
-}
-
-int	main(int argc, char **argv)
-{
-	void	(*ope[11])(void *);
-	char	*operation;
-
-	if (av >1)
-	{ 
-		while (ft_stack_len(stack_b))
-		{
-			operation = get_next_line(0);
-			if (!operation)
-			{
-				ft_putstr_fd("Error\n", 2);
-				exit(EXIT_FAILURE);
-			}
-			*ope[ft_operation_index(operation)](*stack_a, *stack_b);
-			free(operation);
-		}
-		if (ft_issort(stack_a))
-		{
-			ft_putstr_fd("OK\n", 1);
-			ft_exit(1, stack_a, stack_b);
-		}
-		ft_putstr_fd("KO\n", 1);
-		ft_exit(0, stack_a, stack_b);
-	}
-	exit(EXIT_SUCCESS);
-}
+	return (0);
+}	
